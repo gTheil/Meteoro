aad4c2b6deae4cf33266b5b6655a9f1aa5f2ad33
Felipe Theil <lipetheil18@gmail.com>
2021-05-04 22:25:12 -0300

    cenas de level select e creditos criadas

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    public Button levelButton;
+    public Button creditsButton;
+    public Button quitButton;
+    public Button backButton;
+    public Button retryButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
